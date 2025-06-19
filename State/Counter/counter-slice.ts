d7185a653fd7ccc8f3097f4395a2c8723a38a502
Shubhamxshah <ritikgujarat@gmail.com>
2025-06-20 00:52:09 +0530

    async counter slice updated

--- State/Counter/counter-slice.ts
+++ State/Counter/counter-slice.ts
@@ -1,6 +1,6 @@
-'use client'
+"use client";
 
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { PayloadAction } from "@reduxjs/toolkit";
 
 interface CounterState {
@@ -8,24 +8,46 @@ interface CounterState {
 }
 
 const initialState: CounterState = {
-  value: 0
-}
+  value: 0,
+};
 
 const counterSlice = createSlice({
-  name: "counter", 
-  initialState, 
+  name: "counter",
+  initialState,
   reducers: {
     increment: (state) => {
       state.value += 1;
-    }, 
+    },
     decrement: (state) => {
       state.value -= 1;
     },
-    incrementByAmount: (state, action:PayloadAction<number>) =>{
+    incrementByAmount: (state, action: PayloadAction<number>) => {
       state.value += action.payload;
-    }
-  }
-})
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(
+        incrementAsync.pending, () => {
+          console.log("hi shubham")
+        }
+      )
+      .addCase(
+      incrementAsync.fulfilled,
+      (state, action: PayloadAction<number>) => {
+        state.value -= action.payload;
+      },
+    );
+  },
+});
 
-export const {increment, decrement, incrementByAmount} = counterSlice.actions;
+export const incrementAsync = createAsyncThunk(
+  "counter/incrementAsync",
+  async (amount: number) => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return amount;
+  },
+);
+
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 export default counterSlice.reducer; // available from createSlice
--- components/Counter.tsx
+++ components/Counter.tsx
@@ -1,21 +1,22 @@
 'use client'
 
-import { decrement, increment, incrementByAmount } from '@/State/Counter/counter-slice';
-import { RootState } from '@/State/store'
+import { decrement, increment, incrementAsync, incrementByAmount } from '@/State/Counter/counter-slice';
+import { AppDispatch, RootState } from '@/State/store'
 import React from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 const Counter = () => {
 
   const count = useSelector((state: RootState) => state.counter.value);
-  const dispatch = useDispatch();
+  const dispatch = useDispatch<AppDispatch>();
 
   return (
     <div>
     <h2>{count}</h2>
       <button onClick={() => dispatch(increment())}> Increment</button>
       <button onClick={() => dispatch(decrement())}> Decrement</button>
-      <button onClick={() => dispatch(incrementByAmount(10))}> Increment 10</button>  
+      <button onClick={() => dispatch(incrementByAmount(10))}> Increment 10</button>
+      <button onClick={() => dispatch(incrementAsync(5))}>Decrement 5</button>
     </div>
   )
 }
